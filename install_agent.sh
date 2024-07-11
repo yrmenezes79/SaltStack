15ef0443a802d68240395592c0c4bdd483ea9de8
yrmenezes79 <yrmenezes@gmail.com>
2024-07-11 13:41:16 -0300

    install

--- install_agent.sh
+++ install_agent.sh
@@ -12,6 +12,21 @@ check_return_code() {
     fi
 }
 
+
+function validar_ip() {
+    local ip=$1
+    local stat=1
+
+    if [[ $ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
 FASE="Inserir repositório"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - $FASE"
@@ -28,4 +43,19 @@ check_return_code "$FASE"
 systemctl enable salt-minion && systemctl start salt-minion
 check_return_code "$FASE"
 
+FASE="Validar IP"
+# Solicita o nome
+read -p "Digite o nome: " nome
 
+# Solicita o IP
+read -p "Digite o IP: " ip
+
+# Valida o IP
+if validar_ip $ip; then
+    echo "$ip    $nome" | sudo tee -a /etc/hosts > /dev/null
+    check_return_code "$FASE"
+    sed -i 's/#master: salt/master $nome/g' /etc/salt/minion
+    check_return_code "$FASE"
+    systemctl restart salt-minion 
+    check_return_code "$FASE"
+fi
